5d6ab7dfa3b5db4f32e81eb39e76ebe51bb2ad38
Bruno Oliveira de Souza <brunosouza22397@gmail.com>
2021-03-18 22:20:06 -0300

    conversao de x para string movida para func

--- calculo-secante.py
+++ calculo-secante.py
@@ -1,5 +1,6 @@
 #recebe valor x e funcao (ex.: x³+2x²+1) e retorna o valor de f(x)
 def funcao(x, func):
+    x = str(x)
     func = func.replace("x",x)
     result = eval(func)
     return result;
@@ -7,19 +8,19 @@ def funcao(x, func):
 #calcula raiz de função utilizando o método de secante
 #recebe x0 e x1 como valores iniciais, TOL como critério de parada, N como número de repetições e a função que será calculada.
 def secante(x0, x1, TOL, N, func):
-    x2 = x1 - (x1 - x0)*funcao(str(x1), func)/(funcao(str(x1), func) - funcao(str(x0), func))
+    x2 = x1 - (x1 - x0)*funcao(x1, func)/(funcao(x1, func) - funcao(x0, func))
     i = 1
     
-    while ((abs(funcao(str(x2), func)) > TOL) and (i<= N)):
+    while ((abs(funcao(x2, func)) > TOL) and (i<= N)):
         x0 = x1
         x1 = x2
-        x2 = x1 - (x1 - x0)*funcao(str(x1), func)/(funcao(str(x1), func) - funcao(str(x0), func))
+        x2 = x1 - (x1 - x0)*funcao(x1, func)/(funcao(x1, func) - funcao(x0, func))
         i = i+1
         
     if (i > N):
         print ('Nao houve convergencia!')
         
-    if (abs(funcao(str(x2), func)) < TOL):
+    if (abs(funcao(x2, func)) < TOL):
         print(x2)
         
 secante(-1.8, -1.2, .1, 20, "x**4-3*x**3+3")
